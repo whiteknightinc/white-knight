838db52972bdbff4839200ff20e575731f64e3b3
Robert Haskell <robertwhaskell@gmail.com>
2015-02-23 11:37:52 -0800

    A little cleanup.

--- newstuff/whiteapp.py
+++ newstuff/whiteapp.py
@@ -1,73 +1,30 @@
-import re
-from bookdb import BookDB
-
-DB = BookDB()
-
-def test():
-    pass
-
-def book(book_id):
-    page = """
-        <h1>{title}</h1>
-        <table>
-            <tr><th>Author</th><td>{author}</td></tr>
-            <tr><th>Publisher</th><td>{publisher}</td></tr>
-            <tr><th>ISBN</th><td>{isbn}</td></tr>
-        </table>
-        <a href="/">Back to the list</a>
-            """
-    book = DB.title_info(book_id)
-    if book is None:
-        raise NameError
-    return page.format(**book)
+from wsgiref.simple_server import make_server
+from pyramid.config import Configurator
+from pyramid.response import Response
+from pyramid.view import view_config
+import jinja2
 
 
-def books():
-    all_books = DB.titles()
-    body = ['<h1>My Bookshelf Edited Again</h1>', '<ul>']
-    item_template = '<li><a href="/book/{id}">{title}</a></li>'
-    for book in all_books:
-        body.append(item_template.format(**book))
-    body.append('</ul>')
-    return '\n'.join(body)
+@view_config(route_name='home', renderer='templates/home.jinja2')
+def home(request):
+    return {}
+
+def hello_world(request):
+    print('Incoming request')
+    return Response('<body><h1>Hello World! What?</h1></body>')
 
 
-def resolve_path(path):
-    urls = [(r'^$', books),
-            (r'^book/(id[\d]+)$', book)]
-    matchpath = path.lstrip('/')
-    for regexp, func in urls:
-        match = re.match(regexp, matchpath)
-        if match is None:
-            continue
-        args = match.groups([])
-        return func, args
-    # we get here if no url matches
-    raise NameError
-
-
-def application(environ, start_response):
-    headers = [("Content-type", "text/html")]
-    try:
-        path = environ.get('PATH_INFO', None)
-        if path is None:
-            raise NameError
-        func, args = resolve_path(path)
-        body = func(*args)
-        status = "200 OK"
-    except NameError:
-        status = "404 Not Found"
-        body = "<h1>Not Found</h1>"
-    except Exception:
-        status = "500 Internal Server Error"
-        body = "<h1>Internal Server Error</h1>"
-    finally:
-        headers.append(('Content-length', str(len(body))))
-        start_response(status, headers)
-        return [body]
-
+def app():
+    config = Configurator()
+    config.include('pyramid_jinja2')
+    config.add_route('home', '/')
+    config.scan()
+    app = config.make_wsgi_app()
+    return app
 
 if __name__ == '__main__':
-    from wsgiref.simple_server import make_server
-    srv = make_server('localhost', 8080, application)
-    srv.serve_forever()
+
+    # config.add_view(hello_world, route_name='hello')
+    app = app()
+    server = make_server('0.0.0.0', 8080, app)
+    server.serve_forever()
