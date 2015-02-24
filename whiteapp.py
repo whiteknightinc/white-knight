46c516586185a10b93588fdc4fb30c698c193c6f
Nick Becker <nbeck90@uw.edu>
2015-02-23 16:42:23 -0800

    App scrapes on load

--- whiteapp.py
+++ whiteapp.py
@@ -1,6 +1,5 @@
 from pyramid.session import SignedCookieSessionFactory
 from pyramid.config import Configurator
-from pyramid.response import Response
 from pyramid.view import view_config
 from pyramid.authentication import AuthTktAuthenticationPolicy
 from pyramid.authorization import ACLAuthorizationPolicy
@@ -47,7 +46,11 @@ class Comments(Base):
             username = comments[comment]['user']
             permalink = comments[comment]['permalink']
             reddit = reddit
-            new_entry = cls(text=text, username=username, reddit=reddit, permalink=permalink)
+            new_entry = cls(text=text,
+                            username=username,
+                            reddit=reddit,
+                            permalink=permalink
+                            )
             DBSession.add(new_entry)
 
     @classmethod
@@ -71,7 +74,7 @@ def main():
     settings['reload_all'] = os.environ.get('DEBUG', True)
     settings['debug_all'] = os.environ.get('DEBUG', True)
     settings['sqlalchemy.url'] = os.environ.get(
-        'DATABASE_URL', 'postgresql://roberthaskell:@localhost:5432/whiteknight'
+        'DATABASE_URL', 'postgresql://nbeck:@localhost:5432/whiteknight'
     )
     engine = sa.engine_from_config(settings, 'sqlalchemy.')
     DBSession.configure(bind=engine)
