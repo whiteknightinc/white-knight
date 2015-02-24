82d4f8afc489eace6d0d8a8cb891001cd6183ca2
Nicholas Draper <ndraper2@gmail.com>
2015-02-24 11:27:40 -0800

    pointscraper now uses DSTK for sentiment analysis

--- pointscraper.py
+++ pointscraper.py
@@ -1,18 +1,20 @@
 import praw
+import dstk
 
 
 def get_comments():
     r = praw.Reddit('Whiteknight scrapping reddit for nasty comments'
                     'Url: https://github.com/whiteknightinc/white-knight')
 
-    top_posts = r.get_subreddit('gaming').get_top(limit=10)
+    top_posts = r.get_subreddit('whiteknighttest').get_top(limit=10)
     comments_with_keywords = []
-    f = open("swearWordsValue.txt")
-    keywords = {}
-    for line in f:
-        word, val = line.rstrip().split(",")
-        keywords[word] = int(val)
-    f.close()
+    toolkit = dstk.DSTK()
+    # f = open("swearWordsValue.txt")
+    # keywords = {}
+    # for line in f:
+    #     word, val = line.rstrip().split(",")
+    #     keywords[word] = int(val)
+    # f.close()
 
     for top_post in top_posts:
         submission = r.get_submission(submission_id=top_post.id)
@@ -21,15 +23,18 @@ def get_comments():
         comments = praw.helpers.flatten_tree(all_comments)
 
         for comment in comments:
-            score = 0
             words = comment.body.lower()
-            for keyword in keywords.keys():
-                count = words.count(keyword)
-                if count > 0:
-                    score += count * (keywords.get(keyword))
-                    if score >= 10:
-                        comments_with_keywords.append(comment)
-                        break
+            response =  toolkit.text2sentiment(words.encode('utf-8'))
+            if response['score'] > -3:
+                print comment.body
+                comments_with_keywords.append(comment)
+            # for keyword in keywords.keys():
+            #     count = words.count(keyword)
+            #     if count > 0:
+            #         score += count * (keywords.get(keyword))
+            #         if score >= 10:
+            #             comments_with_keywords.append(comment)
+            #             break
 
     result = {}
     for num in range(len(comments_with_keywords)):
